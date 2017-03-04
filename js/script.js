c53407f6d14d26b7de628779f85ead240ca18976
Micael Robles <micael.robles@gmail.com>
2017-03-04 16:34:04 -0300

    Sort channels by status.

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function getData(channel){
             if (stream == null){
                 $("#container").append('<div class="stream animated fadeInUp"><div id="img1"><img class="streamimg" src="' + 'https://static-cdn.jtvnw.net/ttv-static/404_preview-320x180.jpg' + '"/></div><div class="infocontainer"><h1 class="title">' + channel + '</h1><p class="status"> '+ 'OFFLINE' +' </p></div></div></div>')
             } else {
-                $("#container").append('<div class="stream animated fadeInUp"><div id="img1"><img class="streamimg" src="' + stream.preview.medium + '"/></div><div class="infocontainer"><h1 class="title">' + stream.channel.display_name + '</h1><p class="game">'+ '<strong>Game:</strong> ' + stream.game + '</p><p class="user"> '+'<a href=' + stream.channel.url + '>' + stream.channel.url + '</a>'+'</p><div class="inlinestatus"><p class="status"> '+ 'ONLINE' +' </p><p class="viewers">'+'Viewers: ' + stream.viewers+' </p></div></div></div>')
+                $("#container").prepend('<div class="stream animated fadeInUp"><div id="img1"><img class="streamimg" src="' + stream.preview.medium + '"/></div><div class="infocontainer"><h1 class="title">' + stream.channel.display_name + '</h1><p class="game">'+ '<strong>Game:</strong> ' + stream.game + '</p><p class="user"> '+'<a href=' + stream.channel.url + '>' + stream.channel.url + '</a>'+'</p><div class="inlinestatus"><p class="status"> '+ 'ONLINE' +' </p><p class="viewers">'+'Viewers: ' + stream.viewers+' </p></div></div></div>')
             }
         }
     })
